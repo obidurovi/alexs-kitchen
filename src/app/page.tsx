d5fe881c65bdd5f19fd78301e5f20414c78b193a
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-05 05:46:12 +0600

    data implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import food from "@/data/food";
 import FoodCard from "./components/FoodCard";
+import { Food } from "@/types";
 
 export default function Home() {
   return (
-    <main className="p-3 p-2">
-      <FoodCard />
+    <main className="p-3">
+      <div className="px-2 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-items-center gap-3">
+        {food?.map((item: Food) => (
+          <FoodCard key={item.name} food={item} />
+        ))}
+      </div>
     </main>
   );
 }
