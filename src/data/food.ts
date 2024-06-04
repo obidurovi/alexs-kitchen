0d51f9010f0653ad935ce305f3570f6428b54789
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-05 05:46:27 +0600

    data file added

---
+++ src/data/food.ts
@@ -0,0 +1,159 @@
+const foods = [
+  {
+    name: "Pizza Margherita",
+    ingredients: [
+      "Pizza dough",
+      "Tomatoes",
+      "Mozzarella cheese",
+      "Basil",
+      "Olive oil",
+      "Salt",
+    ],
+    description:
+      "A classic Italian pizza with a simple yet delicious combination of fresh tomatoes, mozzarella cheese, and basil leaves.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/1501b7a18cab4f97b3cb834157712052.jpg",
+  },
+  {
+    name: "Sushi",
+    ingredients: [
+      "Vinegared rice",
+      "Raw fish (tuna, salmon)",
+      "Seaweed",
+      "Vegetables",
+    ],
+    description:
+      "A Japanese dish consisting of vinegared rice accompanied by various ingredients such as seafood, vegetables, and occasionally tropical fruits.",
+    thumbnail:
+      "https://www.tasteatlas.com/Images/Dishes/fde953e5aa504e8c83a84691582b0399.jpg",
+  },
+  {
+    name: "Burger",
+    ingredients: [
+      "Beef patty",
+      "Hamburger bun",
+      "Lettuce",
+      "Tomato",
+      "Cheese",
+      "Pickles",
+      "Onions",
+      "Condiments",
+    ],
+    description:
+      "A popular American sandwich consisting of a cooked patty of ground meat placed inside a sliced bun, often garnished with various toppings.",
+    thumbnail:
+      "https://www.tasteatlas.com/Images/Dishes/223d2928b1e04701854f387c30d940f0.jpg",
+  },
+  {
+    name: "Pad Thai",
+    ingredients: [
+      "Rice noodles",
+      "Shrimp",
+      "Tofu",
+      "Eggs",
+      "Peanuts",
+      "Bean sprouts",
+      "Lime",
+      "Fish sauce",
+    ],
+    description:
+      "A traditional Thai stir-fried noodle dish known for its balance of sweet, sour, salty, and spicy flavors.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/ad3ef1fb39eb42958e7462961951e45c.jpg",
+  },
+  {
+    name: "Tacos",
+    ingredients: [
+      "Tortillas",
+      "Beef",
+      "Chicken",
+      "Lettuce",
+      "Cheese",
+      "Salsa",
+      "Guacamole",
+    ],
+    description:
+      "A traditional Mexican dish composed of a folded or rolled tortilla filled with various mixtures, such as seasoned meat, beans, lettuce, and cheese.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/967fe03b58464dad9f82d91650d98824.jpg",
+  },
+  {
+    name: "Ramen",
+    ingredients: [
+      "Noodles",
+      "Broth",
+      "Pork",
+      "Egg",
+      "Green onions",
+      "Nori",
+      "Bamboo shoots",
+    ],
+    description:
+      "A Japanese noodle soup consisting of Chinese-style wheat noodles served in a meat or fish-based broth, often flavored with soy sauce or miso, and uses toppings such as sliced pork, nori, and scallions.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/b6b9680a32c84a9381a1ea5f5e529698.jpg",
+  },
+  {
+    name: "Paella",
+    ingredients: [
+      "Rice",
+      "Saffron",
+      "Chicken",
+      "Seafood",
+      "Green beans",
+      "Tomato",
+      "Olive oil",
+    ],
+    description:
+      "A Spanish rice dish originally from Valencia. Paella is one of the best-known dishes in Spanish cuisine and includes a variety of seafood, meat, and vegetables.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/0b456cded8e245acb83036114c806091.jpg",
+  },
+  {
+    name: "Bibimbap",
+    ingredients: [
+      "Rice",
+      "Beef",
+      "Egg",
+      "Vegetables",
+      "Gochujang",
+      "Soy sauce",
+      "Sesame oil",
+    ],
+    description:
+      "A Korean rice dish. The term 'bibim' means mixing various ingredients, while 'bap' refers to rice. Bibimbap is served as a bowl of warm white rice topped with sautéed and seasoned vegetables and gochujang, soy sauce, or doenjang.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/4dd65e828a014639994d77ab09f5dba5.jpg",
+  },
+  {
+    name: "Dim Sum",
+    ingredients: [
+      "Dumpling wrappers",
+      "Pork",
+      "Shrimp",
+      "Vegetables",
+      "Soy sauce",
+    ],
+    description:
+      "A variety of Chinese dishes that are traditionally served with tea. They are made in bite-sized portions and are often served in small steamer baskets or on small plates.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/a000ac2b437b4c5aae6ca6b6b8ac384f.jpg",
+  },
+  {
+    name: "Falafel",
+    ingredients: [
+      "Chickpeas",
+      "Garlic",
+      "Onion",
+      "Parsley",
+      "Cumin",
+      "Coriander",
+    ],
+    description:
+      "A deep-fried ball or patty made from ground chickpeas, fava beans, or both. Falafel is a traditional Middle Eastern food that is often served in a pita, which acts as a pocket, or wrapped in a flatbread.",
+    thumbnail:
+      "https://www.tasteatlas.com/images/dishes/6a7d870a48e34847b06f1711907ccf8a.jpg",
+  },
+];
+
+export default foods;
