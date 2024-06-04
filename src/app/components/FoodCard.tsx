f662c3036250e85e21e06b4b5eac1600224a0467
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-05 05:45:37 +0600

    FoodCard Error Solved

--- src/app/components/FoodCard.tsx
+++ src/app/components/FoodCard.tsx
@@ -1,51 +1,52 @@
 "use client";
 
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import { MdFavorite } from "react-icons/md";
-import { IoMdShare } from "react-icons/io";
-import { FaExpandAlt } from "react-icons/fa";
-import { IoIosMore } from "react-icons/io";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { Food } from "@/types";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
 
-export default function RecipeReviewCard() {
+export default function FoodCard({ food }: { food: Food }) {
   return (
-    <Card sx={{ maxWidth: 345, bgcolor: "rgba(218, 211, 190, 0.4)" }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        bgcolor: "rgba(218, 211, 190, 0.4)",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <CardHeader
         action={<IconButton aria-label="settings"></IconButton>}
-        title="Shrimp and Chorizo Paella"
+        title={food.name}
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="https://cdn.shopify.com/s/files/1/0070/7032/files/food-photgraphy-tips.png?v=1657891849"
-      />
+      <CardMedia component="img" height="194" image={food.thumbnail} />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {food.description}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
+      <CardActions
+        className="flex px-4 mb-2"
+        disableSpacing
+        sx={{ mt: "auto" }}
+      >
         <IconButton aria-label="add to favorites">
-          <MdFavorite />
+          <FaRegEdit />
         </IconButton>
         <IconButton aria-label="share">
-          <IoMdShare />
+          <MdDelete />
         </IconButton>
       </CardActions>
     </Card>
