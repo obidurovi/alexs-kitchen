dbfe0bed333e823ee4a6c49bc9223e8da7ce36a7
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-05 06:02:16 +0600

    Added "Add Item" button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@ import Image from "next/image";
 import food from "@/data/food";
 import FoodCard from "./components/FoodCard";
 import { Food } from "@/types";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <main className="p-3">
+    <main className="py-6 px-4 flex flex-col gap-5">
+      <div className="flex justify-center items-center ">
+        <Button
+          className="w-1/3"
+          color="primary"
+          size="large"
+          fullWidth
+          variant="contained"
+        >
+          Add Food
+        </Button>
+      </div>
       <div className="px-2 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-items-center gap-3">
         {food?.map((item: Food) => (
           <FoodCard key={item.name} food={item} />
