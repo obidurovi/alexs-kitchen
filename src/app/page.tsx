44cd97343e485fc3913d6ec731ddde02be16d777
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-07 22:52:50 +0600

    import AddList Modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,13 @@ import Image from "next/image";
 import food from "@/data/food";
 import FoodCard from "../components/FoodCard";
 import { Food } from "@/types";
-import { Button } from "@mui/material";
+import AddList from "@/components/Modal/AddList";
 
 export default function Home() {
   return (
     <main className="py-6 px-4 flex flex-col gap-5">
       <div className="flex justify-start items-center ">
-        <Button
-          className="w-1/6"
-          color="primary"
-          size="large"
-          fullWidth
-          variant="contained"
-        >
-          Add Food
-        </Button>
+        <AddList />
       </div>
       <div className="px-2 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-items-center gap-3">
         {food?.map((item: Food) => (
