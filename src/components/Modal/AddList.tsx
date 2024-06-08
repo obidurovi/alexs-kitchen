516ae174d1a422270ad7037573a6b5df4a4025b9
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-08 21:55:00 +0600

    remove unnecessary code

--- src/components/Modal/AddList.tsx
+++ src/components/Modal/AddList.tsx
@@ -4,56 +4,8 @@ import * as React from "react";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import Typography from "@mui/material/Typography";
-
-import { useSpring, animated } from "@react-spring/web";
 import { Button, IconButton } from "@mui/material";
 import axios from "axios";
-import swal from "sweetalert2";
-
-interface FadeProps {
-  children: React.ReactElement;
-  in?: boolean;
-  onClick?: any;
-  onEnter?: (node: HTMLElement, isAppearing: boolean) => void;
-  onExited?: (node: HTMLElement, isAppearing: boolean) => void;
-  ownerState?: any;
-}
-
-const Fade = React.forwardRef<HTMLDivElement, FadeProps>(function Fade(
-  props,
-  ref
-) {
-  const {
-    children,
-    in: open,
-    onClick,
-    onEnter,
-    onExited,
-    ownerState,
-    ...other
-  } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter(null as any, true);
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited(null as any, true);
-      }
-    },
-  });
-
-  return (
-    <animated.div ref={ref} style={style} {...other}>
-      {React.cloneElement(children, { onClick })}
-    </animated.div>
-  );
-});
 
 const style = {
   position: "absolute" as "absolute",
@@ -122,84 +74,74 @@ export default function AddList() {
         onClose={handleClose}
         closeAfterTransition
         slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            TransitionComponent: Fade,
-          },
-        }}
       >
-        <Fade in={open}>
-          <Box sx={style} className="box-class  ">
-            <div className="flex flex-col justify-center w-full">
-              <form onSubmit={handleFoodsForm}>
-                <h1
-                  id="spring-modal-title"
-                  className="text-3xl p-4 text-center "
+        <Box sx={style} className="box-class  ">
+          <div className="flex flex-col justify-center w-full">
+            <form onSubmit={handleFoodsForm}>
+              <h1 id="spring-modal-title" className="text-3xl p-4 text-center ">
+                Add Your Food
+              </h1>
+              <div className="mb-5">
+                <label
+                  htmlFor="base-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Add Your Food
-                </h1>
-                <div className="mb-5">
-                  <label
-                    htmlFor="base-input"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Food Name
-                  </label>
-                  <input
-                    type="text"
-                    value={input.name}
-                    onChange={handleInputChange}
-                    name="name"
-                    id="base-input"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  />
-                </div>
-                <div className="mb-5">
-                  <label
-                    htmlFor="base-input"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Short Description
-                  </label>
-                  <input
-                    type="text"
-                    value={input.description}
-                    onChange={handleInputChange}
-                    name="description"
-                    id="base-input"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  />
-                </div>
+                  Food Name
+                </label>
+                <input
+                  type="text"
+                  value={input.name}
+                  onChange={handleInputChange}
+                  name="name"
+                  id="base-input"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="base-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Short Description
+                </label>
+                <input
+                  type="text"
+                  value={input.description}
+                  onChange={handleInputChange}
+                  name="description"
+                  id="base-input"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
 
-                <div className="mb-5">
-                  <label
-                    htmlFor="base-input"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Food Image Url
-                  </label>
-                  <input
-                    type="text"
-                    value={input.thumbnail}
-                    onChange={handleInputChange}
-                    name="thumbnail"
-                    id="base-input"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  />
-                </div>
-                <div className="mb-5">
-                  <Button
-                    type="submit"
-                    id="base-input"
-                    className="add-button-form  text-gray-900 text-sm rounded-lg block w-full p-2.5 "
-                  >
-                    Add Food
-                  </Button>
-                </div>
-              </form>
-            </div>
-          </Box>
-        </Fade>
+              <div className="mb-5">
+                <label
+                  htmlFor="base-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Food Image Url
+                </label>
+                <input
+                  type="text"
+                  value={input.thumbnail}
+                  onChange={handleInputChange}
+                  name="thumbnail"
+                  id="base-input"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+              <div className="mb-5">
+                <Button
+                  type="submit"
+                  id="base-input"
+                  className="add-button-form  text-gray-900 text-sm rounded-lg block w-full p-2.5 "
+                >
+                  Add Food
+                </Button>
+              </div>
+            </form>
+          </div>
+        </Box>
       </Modal>
     </div>
   );
