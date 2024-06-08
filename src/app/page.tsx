b9a54d5645a044cbe6cc1ce427d2f7f34ce9aae3
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-08 21:54:20 +0600

    loader added on the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,27 @@ import { Food } from "@/types";
 import AddList from "@/components/Modal/AddList";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import FoodSkeletons from "@/components/skeleton/FoodSkeleton";
 
 export default function Home() {
   const [foods, setFoods] = useState<Food[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Fetch the foods data from the server
     const fetchFoods = async () => {
       const response = await axios.get("http://localhost:5050/foods");
       setFoods(response.data);
+      setLoading(false);
     };
 
     fetchFoods();
   }, []);
 
+  // handle delete data
   const handleDataDelete = async (id: number) => {
+    console.log(id);
+
     try {
       const response = await axios.delete(`http://localhost:5050/foods/${id}`);
       if (response.status === 200) {
@@ -41,6 +47,7 @@ export default function Home() {
       <div className="flex justify-start items-center ">
         <AddList />
       </div>
+      {loading && <FoodSkeletons />}
       <div className="px-2 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-items-center gap-3">
         {foods.map((food) => (
           <FoodCard key={food.id} food={food} onDelete={handleDataDelete} />
