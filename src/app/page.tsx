bd620aefa8324b3120d894bb1bd9c2d05caa9d08
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-08 16:49:13 +0600

    delete feature added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,43 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const [foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState<Food[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5050/foods").then((res) => {
-      setFoods(res.data);
-    });
-  }, [foods]);
+    // Fetch the foods data from the server
+    const fetchFoods = async () => {
+      const response = await axios.get("http://localhost:5050/foods");
+      setFoods(response.data);
+    };
+
+    fetchFoods();
+  }, []);
+
+  const handleDataDelete = async (id: number) => {
+    try {
+      const response = await axios.delete(`http://localhost:5050/foods/${id}`);
+      if (response.status === 200) {
+        setFoods((foods) => foods.filter((food) => food.id !== id));
+        console.log(`Food with id ${id} deleted successfully.`);
+      } else {
+        console.error(`Failed to delete food with id ${id}`);
+      }
+    } catch (error) {
+      console.error(
+        `An error occurred while deleting the food with id ${id}:`,
+        error
+      );
+    }
+  };
+
   return (
     <main className="py-6 px-4 flex flex-col gap-5">
       <div className="flex justify-start items-center ">
         <AddList />
       </div>
       <div className="px-2 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-items-center gap-3">
-        {foods?.map((item: Food) => (
-          <FoodCard key={item.name} food={item} />
+        {foods.map((food) => (
+          <FoodCard key={food.id} food={food} onDelete={handleDataDelete} />
         ))}
       </div>
     </main>
