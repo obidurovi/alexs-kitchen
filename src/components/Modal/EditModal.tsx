67f5af9fbfefd10c3cb34ebe4cd547276174a080
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-08 21:55:19 +0600

    remove unnecessary code from EditModal

--- src/components/Modal/EditModal.tsx
+++ src/components/Modal/EditModal.tsx
@@ -4,55 +4,10 @@ import * as React from "react";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-
-import { useSpring, animated } from "@react-spring/web";
-import { IconButton } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import { Food } from "@/types";
 import { FaRegEdit } from "react-icons/fa";
-
-interface FadeProps {
-  children: React.ReactElement;
-  in?: boolean;
-  onClick?: any;
-  onEnter?: (node: HTMLElement, isAppearing: boolean) => void;
-  onExited?: (node: HTMLElement, isAppearing: boolean) => void;
-  ownerState?: any;
-}
-
-const Fade = React.forwardRef<HTMLDivElement, FadeProps>(function Fade(
-  props,
-  ref
-) {
-  const {
-    children,
-    in: open,
-    onClick,
-    onEnter,
-    onExited,
-    ownerState,
-    ...other
-  } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter(null as any, true);
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited(null as any, true);
-      }
-    },
-  });
-
-  return (
-    <animated.div ref={ref} style={style} {...other}>
-      {React.cloneElement(children, { onClick })}
-    </animated.div>
-  );
-});
+import axios from "axios";
 
 const style = {
   position: "absolute" as "absolute",
@@ -74,8 +29,34 @@ const style = {
 
 export default function EditModal({ food }: { food: Food }) {
   const [open, setOpen] = React.useState(false);
+  const [editedFood, setEditedFood] = React.useState<Food>(food);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  // handle edit data
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setEditedFood((prevFood) => ({
+      ...prevFood,
+      [id]: value,
+    }));
+  };
+
+  // handle submit form
+  const handleEditForm = (e) => {
+    e.preventDefault();
+    if (editedFood.name || editedFood.description || editedFood.thumbnail) {
+      axios
+        .put(`http://localhost:5050/foods/${food.id}`, editedFood)
+        .then((res) => {
+          setEditedFood(res.data);
+          handleClose();
+        })
+        .catch((error) => {
+          console.error("Error updating food:", error);
+        });
+    }
+  };
 
   return (
     <div>
@@ -89,15 +70,10 @@ export default function EditModal({ food }: { food: Food }) {
         onClose={handleClose}
         closeAfterTransition
         slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            TransitionComponent: Fade,
-          },
-        }}
       >
         <Box sx={style}>
           <div className="flex flex-col justify-center ">
-            <form>
+            <form onSubmit={handleEditForm}>
               <h1 id="spring-modal-title" className="text-3xl p-4 text-center ">
                 Edit Your Food Data
               </h1>
@@ -111,7 +87,9 @@ export default function EditModal({ food }: { food: Food }) {
                 </label>
                 <input
                   type="text"
-                  id="base-input"
+                  id="name"
+                  value={editedFood.name}
+                  onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
@@ -124,7 +102,9 @@ export default function EditModal({ food }: { food: Food }) {
                 </label>
                 <input
                   type="text"
-                  id="base-input"
+                  id="description"
+                  value={editedFood.description}
+                  onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
@@ -137,10 +117,19 @@ export default function EditModal({ food }: { food: Food }) {
                 </label>
                 <input
                   type="text"
-                  id="base-input"
+                  id="imageUrl"
+                  value={editedFood.thumbnail}
+                  onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
+              <Button
+                type="submit"
+                id="base-input"
+                className="add-button-form  text-gray-900 text-sm rounded-lg block w-full p-2.5 "
+              >
+                Save Changes
+              </Button>
             </form>
           </div>
         </Box>
