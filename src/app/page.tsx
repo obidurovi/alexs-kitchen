eb92befe44996d69b3d142804f2b965ad27a9603
MD Obidur Rahman Bhuiyan <mdobidurrahmanbhuiyan@gmail.com>
2024-06-08 13:00:26 +0600

    get all data by from json api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
-import Image from "next/image";
-import food from "@/data/food";
+"use client";
+
 import FoodCard from "../components/FoodCard";
 import { Food } from "@/types";
 import AddList from "@/components/Modal/AddList";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5050/foods").then((res) => {
+      setFoods(res.data);
+    });
+  }, [foods]);
   return (
     <main className="py-6 px-4 flex flex-col gap-5">
       <div className="flex justify-start items-center ">
         <AddList />
       </div>
       <div className="px-2 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-items-center gap-3">
-        {food?.map((item: Food) => (
+        {foods?.map((item: Food) => (
           <FoodCard key={item.name} food={item} />
         ))}
       </div>
